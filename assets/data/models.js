a3aed28178e61179436d4709b37e014aad8e8089
obiyo1000 <obiyo1000@gmail.com>
2026-05-03 12:37:10 +0700

    Add models for various analyzers

---
+++ assets/data/models.js
@@ -0,0 +1,72 @@
+const models = [
+  {
+    id: 1,
+    name: "XN-10",
+    brand: "Sysmex",
+    image: "assets/img/xn10.jpg",
+    description: "Hematology Analyzer"
+  },
+  {
+    id: 2,
+    name: "DI-60",
+    brand: "Sysmex",
+    image: "assets/img/di60.jpg",
+    description: "Digital Imaging Analyzer"
+  },
+  {
+    id: 3,
+    name: "Atellica CI1900",
+    brand: "Siemens",
+    image: "assets/img/ci1900.jpg",
+    description: "Chemistry Immunoassay Analyzer"
+  },
+  {
+    id: 4,
+    name: "UN3000",
+    brand: "Sysmex",
+    image: "assets/img/un3000.jpg",
+    description: "Urinalysis System"
+  },
+  {
+    id: 5,
+    name: "NX600",
+    brand: "Fujifilm",
+    image: "assets/img/nx600.jpg",
+    description: "Dry Chemistry Analyzer"
+  },
+  {
+    id: 6,
+    name: "DF-55",
+    brand: "Dymind",
+    image: "assets/img/df55.jpg",
+    description: "Hematology Analyzer"
+  },
+  {
+    id: 7,
+    name: "DF-56",
+    brand: "Dymind",
+    image: "assets/img/df56.jpg",
+    description: "Hematology Analyzer"
+  },
+  {
+    id: 8,
+    name: "H-500",
+    brand: "Dirui",
+    image: "assets/img/h500.jpg",
+    description: "Hematology Analyzer"
+  },
+  {
+    id: 9,
+    name: "RU-20",
+    brand: "Sysmex",
+    image: "assets/img/ru20.jpg",
+    description: "Urine Analyzer"
+  },
+  {
+    id: 10,
+    name: "CH930",
+    brand: "Siemens",
+    image: "assets/img/ch930.jpg",
+    description: "Chemistry Analyzer"
+  }
+];
