389ade4289fb8e840124b28cecf6f97f57aeb7ad
obiyo1000 <obiyo1000@gmail.com>
2026-05-03 12:57:32 +0700

    Update models with new device information

--- assets/data/models.js
+++ assets/data/models.js
@@ -1,72 +1,72 @@
 const models = [
   {
     id: 1,
-    name: "XN-10",
-    brand: "Sysmex",
-    image: "assets/img/xn10.jpg",
+    name: "DH-615",
+    brand: "Dymind",
+    image: "assets/img/DH-615.jpg",
     description: "Hematology Analyzer"
   },
   {
     id: 2,
-    name: "DI-60",
+    name: "XR-3000",
     brand: "Sysmex",
-    image: "assets/img/di60.jpg",
-    description: "Digital Imaging Analyzer"
+    image: "assets/img/XR-3000.jpg",
+    description: "Hematology Analyzer"
   },
   {
     id: 3,
     name: "Atellica CI1900",
     brand: "Siemens",
-    image: "assets/img/ci1900.jpg",
+    image: "assets/img/CI1900.jpg",
     description: "Chemistry Immunoassay Analyzer"
   },
   {
     id: 4,
-    name: "UN3000",
+    name: "UN3111",
     brand: "Sysmex",
-    image: "assets/img/un3000.jpg",
+    image: "assets/img/UN3111.jpg",
     description: "Urinalysis System"
   },
   {
     id: 5,
-    name: "NX600",
+    name: "Dri-Chem NX600",
     brand: "Fujifilm",
-    image: "assets/img/nx600.jpg",
+    image: "assets/img/NX600.jpg",
     description: "Dry Chemistry Analyzer"
   },
   {
     id: 6,
-    name: "DF-55",
-    brand: "Dymind",
-    image: "assets/img/df55.jpg",
-    description: "Hematology Analyzer"
+    name: "LA60SE",
+    brand: "Dirui",
+    image: "assets/img/LA60SE.jpg",
+    description: "Chemistry Immunoassay Analyzerr"
   },
   {
     id: 7,
-    name: "DF-56",
-    brand: "Dymind",
-    image: "assets/img/df56.jpg",
-    description: "Hematology Analyzer"
+    name: "H-500",
+    brand: "Dirui",
+    image: "assets/img/H-500.jpg",
+    description: "Urinalysis System"
   },
   {
     id: 8,
-    name: "H-500",
-    brand: "Dirui",
-    image: "assets/img/h500.jpg",
-    description: "Hematology Analyzer"
+    name: "Immu F6",
+    brand: "Medcaptain",
+    image: "assets/img/F6.jpg",
+    description: "Urinalysis System"
   },
   {
     id: 9,
-    name: "RU-20",
-    brand: "Sysmex",
-    image: "assets/img/ru20.jpg",
-    description: "Urine Analyzer"
+    name: "Acusera",
+    brand: "Randox",
+    image: "assets/img/Acusera.jpg",
+    description: "Quality Controls"
   },
   {
     id: 10,
-    name: "CH930",
-    brand: "Siemens",
-    image: "assets/img/ch930.jpg",
-    description: "Chemistry Analyzer"
+    name: "Sciendox 5A",
+    brand: "Improve",
+    image: "assets/img/5A.jpg",
+    description: "Feces Analysis"
   }
 ];
