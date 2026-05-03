0b53d2e13972de31730cfe8632d1b7cf8198d341
obiyo1000 <obiyo1000@gmail.com>
2026-05-03 14:02:19 +0700

    Update models.js

--- assets/data/models.js
+++ assets/data/models.js
@@ -4,7 +4,7 @@ const models = [
     name: "DH-615",
     brand: "Dymind",
     image: "assets/img/DH-615.jpg",
-    description: "Hematology Analyzer"
+    description: "Hematology Analyzer", // เพิ่ม comma
     catalog: "catalog/DH-615.pdf",
     video: "video/DH-615.mp4",
     spec: "spec/DH-615.html"
@@ -14,7 +14,7 @@ const models = [
     name: "XR-3000",
     brand: "Sysmex",
     image: "assets/img/XR-3000.jpg",
-    description: "Hematology Analyzer"
+    description: "Hematology Analyzer",
     catalog: "catalog/XR-3000.pdf",
     video: "video/XR-3000.mp4",
     spec: "spec/XR-3000.html"
@@ -24,7 +24,7 @@ const models = [
     name: "Atellica CI1900",
     brand: "Siemens",
     image: "assets/img/CI1900.jpg",
-    description: "Chemistry Immunoassay Analyzer"
+    description: "Chemistry Immunoassay Analyzer",
     catalog: "catalog/CI1900.pdf",
     video: "video/CI1900.mp4",
     spec: "spec/CI1900.html"
@@ -34,7 +34,7 @@ const models = [
     name: "UN3111",
     brand: "Sysmex",
     image: "assets/img/UN3111.jpg",
-    description: "Urinalysis System"
+    description: "Urinalysis System",
     catalog: "catalog/UN3111.pdf",
     video: "video/UN3111.mp4",
     spec: "spec/UN3111.html"
@@ -44,7 +44,7 @@ const models = [
     name: "Dri-Chem NX600",
     brand: "Fujifilm",
     image: "assets/img/NX600.jpg",
-    description: "Dry Chemistry Analyzer"
+    description: "Dry Chemistry Analyzer",
     catalog: "catalog/NX600.pdf",
     video: "video/NX600.mp4",
     spec: "spec/NX600.html"
@@ -54,7 +54,7 @@ const models = [
     name: "LA60SE",
     brand: "Dirui",
     image: "assets/img/LA60SE.jpg",
-    description: "Chemistry Immunoassay Analyzerr"
+    description: "Chemistry Immunoassay Analyzer", // แก้จาก Analyzerr
     catalog: "catalog/LA60SE.pdf",
     video: "video/LA60SE.mp4",
     spec: "spec/LA60SE.html"
@@ -64,7 +64,7 @@ const models = [
     name: "H-500",
     brand: "Dirui",
     image: "assets/img/H-500.jpg",
-    description: "Urinalysis System"
+    description: "Urinalysis System",
     catalog: "catalog/H-500.pdf",
     video: "video/H-500.mp4",
     spec: "spec/H-500.html"
@@ -74,7 +74,7 @@ const models = [
     name: "Immu F6",
     brand: "Medcaptain",
     image: "assets/img/F6.jpg",
-    description: "Urinalysis System"
+    description: "Urinalysis System",
     catalog: "catalog/F6.pdf",
     video: "video/F6.mp4",
     spec: "spec/F6.html"
@@ -84,7 +84,7 @@ const models = [
     name: "Acusera",
     brand: "Randox",
     image: "assets/img/Acusera.jpg",
-    description: "Quality Controls"
+    description: "Quality Controls",
     catalog: "catalog/Acusera.pdf",
     video: "video/Acusera.mp4",
     spec: "spec/Acusera.html"
@@ -94,7 +94,7 @@ const models = [
     name: "Sciendox 5A",
     brand: "Improve",
     image: "assets/img/5A.jpg",
-    description: "Feces Analysis"
+    description: "Feces Analysis",
     catalog: "catalog/5A.pdf",
     video: "video/5A.mp4",
     spec: "spec/5A.html"
