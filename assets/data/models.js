3a67e9abb293e05f55fb72ad91ee2c494f3baeea
obiyo1000 <obiyo1000@gmail.com>
2026-06-09 15:47:53 +0700

    Update models.js

--- assets/data/models.js
+++ assets/data/models.js
@@ -133,7 +133,7 @@ const models = [
     description: "Blood gases",
     catalog: "NOVA Stat Profile Prime Plus Brochure.pdf",
     video: "video/VDO Prime Plus.mp4",
-    spec: "spec/Specification Analyzer of NOVA Stat Profile Prime Plus.doc",
+    spec: "spec/prime_plus_spec.html",
     glb: "models/Prime Plus.glb",
     usdz: "models/Prime Plus.usdz"
   }
